a85c561cbfbbf5d002ca86dfac2f9182e1d605d8
Justin Johnson <jumejohn@live.com>
2022-10-21 06:06:39 -0700

    react build successful

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
-  const loggedIn = localStorage.userID;
   const navigate = useNavigate();
 
-  if (loggedIn) {
-    navigate("/profile");
-  }
+  useEffect(() => {
+    const loggedIn = localStorage.userID;
+    if (loggedIn) {
+      navigate("/profile");
+    }
+  });
 
   return (
     <div>
