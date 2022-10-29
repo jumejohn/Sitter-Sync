67235aeaa06082564b5fdbe49abeda232ec27f7b
Justin Johnson <jumejohn@live.com>
2022-10-29 08:13:56 -0700

    env

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI_PROD,
-  TOKEN_SECRET: process.env.TOKEN_SECRET_PROD,
+  MONGODB_URI: process.env.MONGODB_URI,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
 };
