61416912e613ecd1bf28c9a2ab02c893c61e8b6c
Justin Johnson <jumejohn@live.com>
2022-11-02 15:55:20 -0700

    Finish styling events

--- client/src/components/EventInfo.js
+++ client/src/components/EventInfo.js
@@ -58,123 +58,148 @@ const EventInfo = (props) => {
         >
           <div>
             {edit ? (
-              <div className="form-contr">
-                <label htmlFor="title" className="form-label">
-                  Title
-                </label>
-                <input
-                  type="text"
-                  className="form-control form-input"
-                  id="title"
-                  placeholder=" Event Title"
-                  {...register("title")}
-                />
+              <div className="card " style={{ maxWidth: "100%" }}>
+                <div className="row">
+                  <div className="col ">
+                    <div className="form-control">
+                      <label htmlFor="title" className="form-label">
+                        Title
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control form-input"
+                        id="title"
+                        placeholder=" Event Title"
+                        {...register("title")}
+                      />
 
-                <div className="mb-3">
-                  <label htmlFor="description" className="form-label">
-                    Description
-                  </label>
-                  <textarea
-                    type="text area"
-                    className="form-control form-input"
-                    id="description"
-                    placeholder=" Event Description"
-                    {...register("description")}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="startDate" className="form-label">
-                    Start Date
-                  </label>
-                  <input
-                    type="dateTime-local"
-                    className="form-control form-input"
-                    id="startDate"
-                    {...register("startDate")}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="endDate" className="form-label">
-                    End Date
-                  </label>
-                  <input
-                    type="dateTime-local"
-                    className="form-control form-input"
-                    id="endDate"
-                    {...register("endDate")}
-                  />
-                </div>
-                <div>
-                  {props.event.children.map((child) => {
-                    return (
-                      <div className="form-check" key={child._id}>
-                        <input
-                          className="form-check-input form-input"
-                          type="checkbox"
-                          value={child._id}
-                          id="flexCheckDefault"
-                          onChange={handleChange}
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="flexCheckDefault"
-                        >
-                          {child.name}
+                      <div className="mb-3">
+                        <label htmlFor="description" className="form-label">
+                          Description
                         </label>
+                        <textarea
+                          type="text area"
+                          className="form-control form-input"
+                          id="description"
+                          placeholder=" Event Description"
+                          {...register("description")}
+                        />
                       </div>
-                    );
-                  })}
-                </div>
+                      <div className="mb-3">
+                        <label htmlFor="startDate" className="form-label">
+                          Start Date
+                        </label>
+                        <input
+                          type="dateTime-local"
+                          className="form-control form-input"
+                          id="startDate"
+                          {...register("startDate")}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="endDate" className="form-label">
+                          End Date
+                        </label>
+                        <input
+                          type="dateTime-local"
+                          className="form-control form-input"
+                          id="endDate"
+                          {...register("endDate")}
+                        />
+                      </div>
+                      <div>
+                        {props.event.children.map((child) => {
+                          return (
+                            <div className="form-check" key={child._id}>
+                              <input
+                                className="form-check-input form-input"
+                                type="checkbox"
+                                value={child._id}
+                                id="flexCheckDefault"
+                                onChange={handleChange}
+                              />
+                              <label
+                                className="form-check-label"
+                                htmlFor="flexCheckDefault"
+                              >
+                                {child.name}
+                              </label>
+                            </div>
+                          );
+                        })}
+                      </div>
 
-                <div className="mb-3">
-                  <label htmlFor="invitedUsers" className="form-label">
-                    Invite Sitter Email Address
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control form-input"
-                    id="invitedUsers"
-                    placeholder="Invite Sitter Email Address"
-                    {...register("invitedUsers")}
-                  />
-                </div>
-                <div>
-                  <button
-                    onClick={handleSubmit(onSubmit)}
-                    className="form-button"
-                  >
-                    Submit
-                  </button>
-                </div>
+                      <div className="mb-3">
+                        <label htmlFor="invitedUsers" className="form-label">
+                          Invite Sitter Email Address
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control form-input"
+                          id="invitedUsers"
+                          placeholder="Invite Sitter Email Address"
+                          {...register("invitedUsers")}
+                        />
+                      </div>
+                      <div>
+                        <button
+                          onClick={handleSubmit(onSubmit)}
+                          className="form-button"
+                        >
+                          Submit
+                        </button>
+                      </div>
 
-                <button onClick={toggleEdit} className="form-button">
-                  Cancel
-                </button>
+                      <button onClick={toggleEdit} className="form-button">
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             ) : (
-              <div className="content-box">
-                <div className="card-container">
-                  <h2>{event.title}</h2>
-                  <p>
-                    {dayjs(event.startDate).format("MMM D, YYYY h:mm A")} -
-                    {dayjs(event.endDate).format("MMM D, YYYY h:mm A")}
-                  </p>
-                  <p>{event.description}</p>
-                  <div>
-                    {event.children.map((child) => (
-                      <div key={child._id}>
-                        <p>{child.name}</p>
-                        <p>{child.age}</p>
-                        <p>{child.childFacts}</p>
+              <div className="card " style={{ maxWidth: "100%" }}>
+                <div className="row">
+                  <div className="whitespace-container">
+                    <div className="whitespace-container-row">
+                      <div className="content-box">
+                        <h2>{event.title}</h2>
+                        <p>
+                          {dayjs(event.startDate).format("MMM D, YYYY h:mm A")}{" "}
+                          -{dayjs(event.endDate).format("MMM D, YYYY h:mm A")}
+                        </p>
+                        <p>{event.description}</p>
                       </div>
-                    ))}
+                      <div className="content-box left-align-text">
+                        {event.children.map((child) => (
+                          <div key={child._id}>
+                            <p>{child.name}</p>
+                            <p>{child.age}</p>
+                            <p>{child.childFacts}</p>
+                          </div>
+                        ))}
+                        <div>{event.confirmedUsers ? "Confirmed" : null}</div>
+                      </div>
+                      <div>
+                        <div>
+                          <button onClick={toggleModal} className="form-button">
+                            CLOSE
+                          </button>
+                        </div>
+                        <div>
+                          <button onClick={toggleEdit} className="form-button">
+                            Edit Event Details
+                          </button>
+                          <button
+                            onClick={deleteAnEvent}
+                            className="form-button"
+                          >
+                            Delete Event?
+                          </button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div>{event.confirmedUsers ? "Confirmed" : null}</div>
-                </div>
-                <button onClick={toggleEdit}>Edit Event Details</button>
-                <button onClick={toggleModal}>CLOSE</button>
-                <div>
-                  <button onClick={deleteAnEvent}> Delete Event?</button>
                 </div>
               </div>
             )}
