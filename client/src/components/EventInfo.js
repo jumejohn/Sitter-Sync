4155cd6668912590e3bdd9f4559bc9992d43bafb
Justin Johnson <jumejohn@live.com>
2022-11-02 16:44:06 -0700

    clean up edit event modal

--- client/src/components/EventInfo.js
+++ client/src/components/EventInfo.js
@@ -60,99 +60,107 @@ const EventInfo = (props) => {
             {edit ? (
               <div className="card " style={{ maxWidth: "100%" }}>
                 <div className="row">
-                  <div className="col ">
-                    <div className="form-control">
-                      <label htmlFor="title" className="form-label">
-                        Title
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control form-input"
-                        id="title"
-                        placeholder=" Event Title"
-                        {...register("title")}
-                      />
+                  <div className="whitespace-container">
+                    <div className="whitespace-container-row">
+                      <div className="content-box">
+                        <label htmlFor="title" className="form-label">
+                          Title
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control form-input"
+                          id="title"
+                          placeholder=" Event Title"
+                          {...register("title")}
+                        />
+                        <div className="mb-3">
+                          <label htmlFor="description" className="form-label">
+                            Description
+                          </label>
+                          <textarea
+                            type="text area"
+                            className="form-control form-input"
+                            id="description"
+                            placeholder=" Event Description"
+                            {...register("description")}
+                          />
+                        </div>
+                      </div>
+                      <div className="content-box">
+                        <div className="mb-3 ">
+                          <label htmlFor="startDate" className="form-label">
+                            Start Date
+                          </label>
+                          <input
+                            type="dateTime-local"
+                            className="form-control form-input"
+                            id="startDate"
+                            {...register("startDate")}
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <label htmlFor="endDate" className="form-label">
+                            End Date
+                          </label>
+                          <input
+                            type="dateTime-local"
+                            className="form-control form-input"
+                            id="endDate"
+                            {...register("endDate")}
+                          />
+                        </div>
+                      </div>
+                      <div className="content-box">
+                        <div>
+                          {props.event.children.map((child) => {
+                            return (
+                              <div className="form-check" key={child._id}>
+                                <input
+                                  className="form-check-input form-input"
+                                  type="checkbox"
+                                  value={child._id}
+                                  id="flexCheckDefault"
+                                  onChange={handleChange}
+                                />
+                                <label
+                                  className="form-check-label"
+                                  htmlFor="flexCheckDefault"
+                                >
+                                  {child.name}
+                                </label>
+                              </div>
+                            );
+                          })}
+                        </div>
 
-                      <div className="mb-3">
-                        <label htmlFor="description" className="form-label">
-                          Description
-                        </label>
-                        <textarea
-                          type="text area"
-                          className="form-control form-input"
-                          id="description"
-                          placeholder=" Event Description"
-                          {...register("description")}
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <label htmlFor="startDate" className="form-label">
-                          Start Date
-                        </label>
-                        <input
-                          type="dateTime-local"
-                          className="form-control form-input"
-                          id="startDate"
-                          {...register("startDate")}
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <label htmlFor="endDate" className="form-label">
-                          End Date
-                        </label>
-                        <input
-                          type="dateTime-local"
-                          className="form-control form-input"
-                          id="endDate"
-                          {...register("endDate")}
-                        />
+                        <div className="mb-3">
+                          <label htmlFor="invitedUsers" className="form-label">
+                            Invite Sitter Email Address
+                          </label>
+                          <input
+                            type="email"
+                            className="form-control form-input"
+                            id="invitedUsers"
+                            placeholder="Invite Sitter Email Address"
+                            {...register("invitedUsers")}
+                          />
+                        </div>
                       </div>
                       <div>
-                        {props.event.children.map((child) => {
-                          return (
-                            <div className="form-check" key={child._id}>
-                              <input
-                                className="form-check-input form-input"
-                                type="checkbox"
-                                value={child._id}
-                                id="flexCheckDefault"
-                                onChange={handleChange}
-                              />
-                              <label
-                                className="form-check-label"
-                                htmlFor="flexCheckDefault"
-                              >
-                                {child.name}
-                              </label>
-                            </div>
-                          );
-                        })}
+                        <div className="whitespace-container">
+                          <div className="mb-3">
+                            <button
+                              onClick={handleSubmit(onSubmit)}
+                              className="form-button"
+                            >
+                              Submit
+                            </button>
+                          </div>
+                          <button onClick={toggleEdit} className="form-button">
+                            Cancel
+                          </button>
+                        </div>
                       </div>
-
-                      <div className="mb-3">
-                        <label htmlFor="invitedUsers" className="form-label">
-                          Invite Sitter Email Address
-                        </label>
-                        <input
-                          type="email"
-                          className="form-control form-input"
-                          id="invitedUsers"
-                          placeholder="Invite Sitter Email Address"
-                          {...register("invitedUsers")}
-                        />
-                      </div>
-                      <div>
-                        <button
-                          onClick={handleSubmit(onSubmit)}
-                          className="form-button"
-                        >
-                          Submit
-                        </button>
-                      </div>
-
-                      <button onClick={toggleEdit} className="form-button">
-                        Cancel
-                      </button>
                     </div>
                   </div>
                 </div>
